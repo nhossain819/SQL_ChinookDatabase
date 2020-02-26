ea166088a968b973ab5c370447fadcefbfb0bbd2
nhossain819 <43657615+nhossain819@users.noreply.github.com>
2020-02-26 08:03:27 -0500

    Update top_salesman.sql

--- top_salesman.sql
+++ top_salesman.sql
@@ -1,4 +1,5 @@
 Database: https://cdn.sqlitetutorial.net/wp-content/uploads/2018/03/chinook.zip
+Database Schema: https://cdn.sqlitetutorial.net/wp-content/uploads/2018/03/sqlite-sample-database-diagram-color.pdf
 
 -- Find all invoices for the salesman who yielded the greatest profit.
 
