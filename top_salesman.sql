166047b8ba2e8b6b0f4e3d1a342ddf353b615661
nhossain819 <43657615+nhossain819@users.noreply.github.com>
2020-02-25 20:22:54 -0500

    Create top_salesman.sql

---
+++ top_salesman.sql
@@ -0,0 +1,29 @@
+-- Find all invoices for the salesman who yielded the greatest profit.
+
+SELECT *
+FROM invoices AS i
+	LEFT JOIN customers AS c
+		ON i.CustomerId = c.CustomerId
+	LEFT JOIN employees AS e
+		ON e.EmployeeId = c.SupportRepId
+WHERE e.EmployeeId =
+	(
+		WITH top_employee AS
+		(
+			SELECT e.EmployeeId
+				, e.FirstName
+				, e.LastName
+				, SUM(i.total) as total_sales
+			FROM invoices AS i
+				LEFT JOIN customers AS c
+					ON i.CustomerId = c.CustomerId
+				LEFT JOIN employees AS e
+					ON e.EmployeeId = c.SupportRepId
+			GROUP BY e.EmployeeId
+			ORDER BY total_sales DESC
+			LIMIT 1
+		)
+		SELECT EmployeeId
+		FROM top_employee
+	)
+;
